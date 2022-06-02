35fa0ce3a689b7ad42203907a7aff3c9b3e96b22
EngNoga <nogm_mohamed@yahoo.com>
2022-06-02 20:28:21 +0200

    Hosting Project

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,5 @@
 eb init --platform node.js --region us-east-1
 eb create --sample udagram-api-dev
 eb use udagram-api-dev
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB PORT=$PORT POSTGRES_HOST=$POSTGRES_HOST AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION  JWT_SECRET=$JWT_SECRET
 eb deploy udagram-api-dev
